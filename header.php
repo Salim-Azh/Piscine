4623415884c333ff88461df59a70ab097d08a904
LeoMld <46166058+LeoMld@users.noreply.github.com>
2019-10-04 17:45:43 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -23,12 +23,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
     <!-- Bootstrap js -->
-    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-    <!-- Connection à la base de données -->
+  
  
 
     <title>TOEIC</title>
@@ -55,7 +54,7 @@
             <a class="mr-2 nav-link " href="./index.php" >SE CONNECTER</a>
           </li>
 
-          <!-- Menu déroulant au passage de la souris -->
+          <!-- Menu déroulant au passage de la souris peut être utile pour stats -->
           
             
             <li class="mr-2 nav-item " ><a   style= "text-decoration: none; outline:none;" class= "nav-link " href="#" id="menu"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></a>
@@ -79,7 +78,7 @@
             <a class="mr-2 nav-link " href="./menu.php" >CREER UN COMPTE</a>
           </li>
           
-
+          <!-- apparation de certaines fonctionnalités en fonction d'evnts -->
           <?php
             if (isset($_SESSION['user'])){
               echo "<li><a class=\"nav-link ml-5\" href=\"controleur/logout.php\">Se déconnecter</a></li>";
@@ -89,9 +88,7 @@
         
       </div>
     </nav>
-<script type="text/javascript">
 
-</script>
                                               
                                                    
 
