3221474c9a152dc65e09d984361cf7aff8b350ab
Salim_Azh <azharhoussen.salim@gmail.com>
2019-11-11 11:27:53 +0100

    perf :  display questions

--- view/view_addTest.php
+++ view/view_addTest.php
@@ -1,8 +1,6 @@
-<?php 
-	include_once("header.php");
- ?>
+<?php include_once("header.php"); ?>
 
- <div class=" mt-5 container">
+<div class=" mt-5 container">
  	<div class="text-center">
  		<h1 class="font_blue">Créer un TEST TOEIC</h1>
  	</div>
@@ -12,126 +10,43 @@
 			<input type="text" class="form-control" placeholder="Nom du test" id='nameTest' name='nameTest'required />
 		</div>
 	 	<div class="row">
-		 	<div class="col">
 				<?php
-					
-					for ($i = 1; $i <= 50; $i++) {
-					 	echo ('<div class="row" required>
-
-					 		<div><p class="mt-3 mr-2"> Q '.$i.' :</p></div>
-					 		<div class="form-check form-check-inline" >
-							  <input class="form-check-input" name='.$i.' type="radio" id="rep1" value="a" checked >
-							  <label class="form-check-label" for="rep1">A</label>
-							</div>
-							<div class="form-check form-check-inline">
-							  <input class="form-check-input" name='.$i.' type="radio" id="rep2" value="b">
-							  <label class="form-check-label" for="rep2">B</label>
-							</div>
-							<div class="form-check form-check-inline">
-							  <input class="form-check-input" name='.$i.' type="radio" id="rep3" value="c" >
-							  <label class="form-check-label" for="rep3">C</label>
-							</div>
-							<div class="form-check form-check-inline">
-							  <input class="form-check-input" name='.$i.' type="radio" id="rep4" value="d" >
-							  <label class="form-check-label" for="rep4">D</label>
-							</div>
-						</div>');
+					$cpt = 0;
+					for ($i=0; $i < 4; $i++) { 
+						echo "<div class=\"col\">";
+						for ($j = 0; $j < 50; $j++) {
+							$cpt += 1;
+							echo ('<div class="row" required>
+										<div><p class="mt-3 mr-2"> Q '.$cpt.' :</p></div>
+										<div class="form-check form-check-inline" >
+											<input class="form-check-input" name='.$cpt.' type="radio" id="rep1" value="a" checked >
+											<label class="form-check-label" for="rep1">A</label>
+										</div>
+										<div class="form-check form-check-inline">
+											<input class="form-check-input" name='.$cpt.' type="radio" id="rep2" value="b">
+											<label class="form-check-label" for="rep2">B</label>
+										</div>
+										<div class="form-check form-check-inline">
+											<input class="form-check-input" name='.$cpt.' type="radio" id="rep3" value="c" >
+											<label class="form-check-label" for="rep3">C</label>
+										</div>
+										<div class="form-check form-check-inline">
+											<input class="form-check-input" name='.$cpt.' type="radio" id="rep4" value="d" >
+											<label class="form-check-label" for="rep4">D</label>
+										</div>
+									</div>');
+						}
+					   echo "</div>";
 					}
-
-				?>
-			</div>
-			<div class="col">
-				<?php
-					
-					for ($i = 51; $i <= 100; $i++) {
-					 	echo ('<div class="row" required>
-					 		<div><p class="mt-3 mr-2"> Q '.$i.' :</p></div>
-					 		<div class="form-check form-check-inline">
-							  <input class="form-check-input" name='.$i.' type="radio" id="rep1" value="a" checked>
-							  <label class="form-check-label" for="rep1">A</label>
-							</div>
-							<div class="form-check form-check-inline">
-							  <input class="form-check-input" name='.$i.' type="radio" id="rep2" value="b">
-							  <label class="form-check-label" for="rep2">B</label>
-							</div>
-							<div class="form-check form-check-inline">
-							  <input class="form-check-input" name='.$i.' type="radio" id="rep3" value="c" >
-							  <label class="form-check-label" for="rep3">C</label>
-							</div>
-							<div class="form-check form-check-inline">
-							  <input class="form-check-input" name='.$i.' type="radio" id="rep4" value="d" >
-							  <label class="form-check-label" for="rep4">D</label>
-							</div>
-						</div>');
-					}
-
-				?>
-			</div>
-			<div class="col">
-			<?php
-				
-				for ($i = 101; $i <= 150; $i++) {
-				 	echo ('<div class="row" required>
-				 		<div><p class="mt-3 mr-2"> Q '.$i.' :</p></div>
-				 		<div class="form-check form-check-inline">
-						  <input class="form-check-input" name='.$i.' type="radio" id="rep1" value="a" checked>
-						  <label class="form-check-label" for="rep1">A</label>
-						</div>
-						<div class="form-check form-check-inline">
-						  <input class="form-check-input" name='.$i.' type="radio" id="rep2" value="b">
-						  <label class="form-check-label" for="rep2">B</label>
-						</div>
-						<div class="form-check form-check-inline">
-						  <input class="form-check-input" name='.$i.' type="radio" id="rep3" value="c" >
-						  <label class="form-check-label" for="rep3">C</label>
-						</div>
-						<div class="form-check form-check-inline">
-						  <input class="form-check-input" name='.$i.' type="radio" id="rep4" value="d" >
-						  <label class="form-check-label" for="rep4">D</label>
-						</div>
-					</div>');
-				}
-
-			?>
-		</div>
-		<div class="col">
-			<?php
-				
-				for ($i = 151; $i <= 200; $i++) {
-				 	echo ('<div class="row" required>
-				 		<div><p class="mt-3 mr-2"> Q '.$i.' :</p></div>
-				 		<div class="form-check form-check-inline">
-						  <input class="form-check-input" name='.$i.' type="radio" id="rep1" value="a" checked>
-						  <label class="form-check-label" for="rep1">A</label>
-						</div>
-						<div class="form-check form-check-inline">
-						  <input class="form-check-input" name='.$i.' type="radio" id="rep2" value="b">
-						  <label class="form-check-label" for="rep2">B</label>
-						</div>
-						<div class="form-check form-check-inline">
-						  <input class="form-check-input" name='.$i.' type="radio" id="rep3" value="c" >
-						  <label class="form-check-label" for="rep3">C</label>
-						</div>
-						<div class="form-check form-check-inline">
-						  <input class="form-check-input" name='.$i.' type="radio" id="rep4" value="d" >
-						  <label class="form-check-label" for="rep4">D</label>
-						</div>
-					</div>');
-				}
-
-			?>
-		</div>
+				?>	
 		</div>
 		
-
-	<div class="mt-5 mb-5 text-center">
-		<input type="submit" name="ajouter_test" class="btn btn-primary" value="Enregistrer le test" />
-	</div>
+		<div class="mt-5 mb-5 text-center">
+			<input type="submit" name="ajouter_test" class="btn btn-primary" value="Enregistrer le test" />
+		</div>
  	</form>
 
 
- </div>
+</div>
 
- <?php 
-	include_once("footer.php");
- ?>
+ <?php include_once("footer.php");?>
