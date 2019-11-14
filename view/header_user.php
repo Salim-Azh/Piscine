9dbd98b73b88f4aeac4876a4e42621f72c0302f6
tirgue <55982589+tirgue@users.noreply.github.com>
2019-11-14 15:49:20 +0100

    fix : header_user

--- view/header_user.php
+++ view/header_user.php
@@ -2,13 +2,14 @@
 <div class="ml-5 mt-4 collapse navbar-collapse" id="collapse_target" >
 <ul id="menu" class="navbar-nav mr-auto mb-4 ml-5 ">
     <li>
-      <a class="mr-2 nav-link " href="" >Profil</a>
+      <a class="mr-2 nav-link " href="../controller/ctrl_homePage.php" >PROFIL</a>
     </li>
     <li>
-      <a class="mr-2 nav-link " href="" >Statistiques</a>
+      <a class="mr-2 nav-link " href="#" >STATISTIQUES</a>
     </li>
-   
-
-    <li><a class=nav-link ml-5 href=controleur/logout.php>Se déconnecter</a></li>
+   <li>
+      <a class="mr-2 nav-link " href="../controller/ctrl_startTest.php" >DEMARRER UN TEST</a>
+    </li>
+    <li><a class="nav-link" ml-5 href="../controller/logout.php">Se déconnecter</a></li>
  </ul>
 </div>
