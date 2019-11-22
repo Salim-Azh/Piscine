fdd55ca23fc143204c60600d1e0d73c9d558de61
tirgue <55982589+tirgue@users.noreply.github.com>
2019-11-22 18:34:13 +0100

    score convertit en score toeic

--- model/getNoteTotale.php
+++ model/getNoteTotale.php
@@ -1,16 +1,42 @@
 <?php  
 	function getNoteTotale($idTest, $idUser) {
 		require('db.php');
-		$rep = "SELECT score FROM fill WHERE FK_idUser = $idUser and FK_idTest = $idTest";
+		$rep = "SELECT FK_idPart,score FROM fill WHERE FK_idUser = $idUser and FK_idTest = $idTest";
 		$res = mysqli_query($co, $rep) or die('err_getNoteTotale');
 
 		if(mysqli_num_rows($res)!=0){
-            $grp = mysqli_fetch_all($res);
-            $note = 0;
-            for ($i=0; $i < 7; $i++) { 
-            	$note += $grp[$i][0];
+            $score = mysqli_fetch_all($res);
+            $noteListening = 0;
+            $noteReading = 0;
+
+            for ($i=0; $i < 4; $i++) { 
+                $noteListening += $score[$i][1];
             }
-            return $note;
+            for ($i=4; $i < 7; $i++) { 
+                $noteReading += $score[$i][1];
+            }
+
+            if ($noteListening >= 90) {
+                $noteListening = 495;
+            }
+            elseif ($noteListening <= 6) {
+                $noteListening = 5;
+            }
+            else {
+                $noteListening = 495 - (90 - $noteListening) * 5;
+            }
+
+            if ($noteReading >= 97) {
+                $noteReading = 495;
+            }
+            elseif ($noteReading <= 16) {
+                $noteReading = 5;
+            }
+            else {
+                $noteReading = 495 - (97 - $noteReading) * 5;
+            }
+
+            return $noteListening + $noteReading;
         }
         else {
         	return "-";
