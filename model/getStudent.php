5c579ff8d9b6304c2c99c48cc0ababaa0a6eaccc
leo <lele.mollard@gmail.com>
2019-12-03 14:02:43 +0100

    stats student

--- model/getStudent.php
+++ model/getStudent.php
@@ -2,13 +2,15 @@
 //return all the tests existing in the database
 	function getStudent( $spe, $year) {
 		require('db.php');
-		$req = "SELECT user.nameUser , user.firstNameUser FROM user, student, speciality WHERE user.idUser = student.FK_idUser AND student.FK_idSpeciality = speciality.idSpeciality AND speciality.idSpeciality = $spe AND student.yearStu = $year ORDER BY user.firstNameUser, user.nameUser";
-		echo $req;
+		if($spe != "-" and $year != "-"){
+			$req = "SELECT user.nameUser , user.firstNameUser FROM user, student, speciality WHERE user.idUser = student.FK_idUser AND student.FK_idSpeciality = speciality.idSpeciality AND speciality.idSpeciality = $spe AND student.yearStu = $year ORDER BY user.firstNameUser, user.nameUser";
 		$res = mysqli_query($co, $req) or die('err_getStudent');
-		var_dump($res);
 		if (mysqli_num_rows($res) != 0) {
 			$student = mysqli_fetch_all($res);
 			return $student;
 		}
+	}else{
+		return null;
+	}
 	}
 ?>
