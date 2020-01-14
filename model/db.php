f3266588e99b278e8034845ca1e4e7fa51371411
Salim-Azh <37662169+Salim-Azh@users.noreply.github.com>
2020-01-14 12:06:08 +0100

    Update db.php

--- model/db.php
+++ model/db.php
@@ -1,7 +1,7 @@
 <?php
 	//link to database
-	$host = "localhost";
-	$user = "root";
+	$host = "ec2-54-228-237-40.eu-west-1.compute.amazonaws.com:5432";
+	$user = "fgvvlzoxvsztlz";
 	$DB = "mock_toeic";
-    $passwd = "";
+    $passwd = "d65bca6e99f42e34144a0f534a1a4df16650c19ce6586a55747f9d41d9bbe43d";
 	$co = mysqli_connect($host, $user, $passwd, $DB) or die("err_db.php");
