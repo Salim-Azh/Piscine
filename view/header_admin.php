8ded413e641ba3e71671374a9b0e1f4820fb6269
Salim_Azh <azharhoussen.salim@gmail.com>
2020-01-14 19:18:51 +0100

    style : indentation suppression test.php js chart

--- view/header_admin.php
+++ view/header_admin.php
@@ -1,31 +1,32 @@
 <!-- lien de navigation -->
 <div class="ml-5 mt-4 collapse navbar-collapse" id="collapse_target" >
-<ul id="menu" class="navbar-nav mr-auto mb-4 ml-5 ">
+  <ul id="menu" class="navbar-nav mr-auto mb-4 ml-5 ">
     <li>
       <a class="mr-2 nav-link " href="./ctrl_homePage.php" >PROFIL</a>
     </li>
-    <li class="mr-2 nav-item " ><a   style= "text-decoration: none; outline:none;" class= "nav-link " href="#" id="menu"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">STATISTIQUES</a>
-
-            
-            <ul class="shadow">
-              <li class="dropdown-item "><a href="./ctrl_statsbystudent.php" style= "text-decoration: none;" >
-              <h7>par élèves</h7>
-              </a></li>
-              <li class="dropdown-item "><a  href="./ctrl_statsbyclass.php" style= "text-decoration: none;" href="">
-              <h7 >par classe / groupe</h7>
-              </a></li>
-              
-            </ul>
-          
-          </li>
-          <li>
-		    <a class="mr-2 nav-link " href="./ctrl_addTest.php" >CREER UN TEST</a>
-		  </li>
-
-		  <li>
-		    <a class="mr-2 nav-link " href="./ctrl_manageTest.php" >GERER LES TESTS</a>
-		  </li>
-
-    <li><a class=nav-link ml-5 href="./logout.php">Se déconnecter</a></li>
- </ul>
+    <li class="mr-2 nav-item ">
+      <a   style= "text-decoration: none; outline:none;" class= "nav-link " href="#" id="menu"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">STATISTIQUES</a>      
+      <ul class="shadow">
+        <li class="dropdown-item ">
+          <a href="./ctrl_statsbystudent.php" style= "text-decoration: none;">
+            <h7>par élèves</h7>
+          </a>
+        </li>
+        <li class="dropdown-item ">
+          <a  href="./ctrl_statsbyclass.php" style= "text-decoration: none;" href="">
+            <h7 >par classe / groupe</h7>
+          </a> 
+        </li>
+      </ul> 
+    </li>
+    <li>
+		  <a class="mr-2 nav-link " href="./ctrl_addTest.php" >CREER UN TEST</a>
+		</li>
+    <li>
+	    <a class="mr-2 nav-link " href="./ctrl_manageTest.php" >GERER LES TESTS</a>
+	  </li>
+    <li>
+      <a class=nav-link ml-5 href="./logout.php">Se déconnecter</a>
+    </li>
+  </ul>
 </div>
--- view/header_user.php
+++ view/header_user.php
@@ -1,22 +1,22 @@
 <!-- lien de navigation -->
 <div class="ml-5 mt-4 collapse navbar-collapse" id="collapse_target" >
-<ul id="menu" class="navbar-nav mr-auto mb-4 ml-5 ">
-    <li>
-      <a class="mr-2 nav-link " href="../controller/ctrl_homePage.php" >PROFIL</a>
-    </li>
-    <li>
-      <a class="mr-2 nav-link " href="../controller/ctrl_studentStat.php" >STATISTIQUES</a>
-    </li>
-   <li>
-      <a class="mr-2 nav-link " href="../controller/ctrl_startTest.php" >DEMARRER UN TEST</a>
-    </li>
-    <li><a class="nav-link" ml-5 href="../controller/logout.php">Se déconnecter</a></li>
- </ul>
+  <ul id="menu" class="navbar-nav mr-auto mb-4 ml-5 ">
+      <li>
+        <a class="mr-2 nav-link " href="../controller/ctrl_homePage.php" >PROFIL</a>
+      </li>
+      <li>
+        <a class="mr-2 nav-link " href="../controller/ctrl_studentStat.php" >STATISTIQUES</a>
+      </li>
+      <li>
+        <a class="mr-2 nav-link " href="../controller/ctrl_startTest.php" >DEMARRER UN TEST</a>
+      </li>
+      <li>
+        <a class="nav-link" ml-5 href="../controller/logout.php">Se déconnecter</a>
+      </li>
+  </ul>
 
  <!-- AFFICHAGE DU CHRONO LORSQU'UN TEST EST DEMARRE -->
- <body onload="decompte();">
- 
+  <body onload="decompte();">
     <div class="" id="Chrono"></div>
- 
   </body>
 </div>
