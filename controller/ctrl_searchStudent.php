eac6938eef47a9a8714b6c76fcec6fc874fe1617
LeoMld <46166058+LeoMld@users.noreply.github.com>
2020-01-15 19:16:33 +0100

    Update ctrl_searchStudent.php

--- controller/ctrl_searchStudent.php
+++ controller/ctrl_searchStudent.php
@@ -4,9 +4,9 @@
 	require_once('../model/security.php');
 	require_once('../model/getSpeciality.php');
 	require_once('../model/getStudent.php');
+	//on récupère la specialité, l'année des étudiants recherchés pour ensuite les chercher dans la bdd (avec getstudent()) et les afficher dans la vue
 	$spe = getSpeciality();
-	
 	$speChoice = Security::bdd($_POST['speChoice']);
-    $year = Security::bdd($_POST['year']);
-    $name = getStudent($speChoice, $year);
+    	$year = Security::bdd($_POST['year']);
+    	$name = getStudent($speChoice, $year);
 	include_once('../view/view_statsbystudent.php');
