1cfef9b0b23e978a7566901bae6b975e1c5f9b1f
LeoMld <46166058+LeoMld@users.noreply.github.com>
2020-01-15 19:03:47 +0100

    Update ctrl_changePassword.php

--- controller/ctrl_changePassword.php
+++ controller/ctrl_changePassword.php
@@ -4,6 +4,8 @@
 	require_once('../model/security.php');
 	require_once('../model/user.php');
 
+	//on teste si tous les champs on bien été reçu et on compare les nouveaux entre eux et l'ancien avec celui stocké en bdd
+	//on affiche ensuite un message en fonction
 	if(isset($_POST['mdp1']) and isset($_POST['mdp2']) and isset($_POST['mdp0'])){
 		$mdp0 = $_POST['mdp0'];
 		$mdp1 = $_POST['mdp1'];
