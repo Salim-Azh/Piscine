09ef7d902a6c0fff5f68f18124f116ba050784e7
LeoMld <46166058+LeoMld@users.noreply.github.com>
2020-01-15 19:14:09 +0100

    Update ctrl_saveEditTest.php

--- controller/ctrl_saveEditTest.php
+++ controller/ctrl_saveEditTest.php
@@ -6,7 +6,7 @@
 	$nameTest = Security::bdd($_POST['nameTest']);
 	$idTest = Security::bdd($_POST['idTest']);
 	$tabChoix = [];
-	
+	//on récupère toutes les nouvelles questions et on recréé le test par dessus l'ancien
 	for ($i = 1; $i <= 200; $i++) {
 		array_push($tabChoix, Security::bdd($_POST[$i]));
 	}
