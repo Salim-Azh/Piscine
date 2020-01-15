d2fedb7f9e40bbc6c57faddc96060a81392e14a8
LeoMld <46166058+LeoMld@users.noreply.github.com>
2020-01-15 19:18:11 +0100

    Update ctrl_startTest.php

--- controller/ctrl_startTest.php
+++ controller/ctrl_startTest.php
@@ -4,7 +4,7 @@
 	require_once('../model/security.php');
 	require_once('../model/getTest.php');
 	require_once('../model/getNoteTotale.php');
-
+	//on récupère le test et on vérifie si un test a été ouvert pour pouvoir afficher le code dans la vue ensuite
 	$test = getTest();
 
 	if (isset($_POST['code']) ){
