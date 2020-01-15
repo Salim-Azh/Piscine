6b5dc3d6d71dd7b9ea40867e0982b7c95e23d698
LeoMld <46166058+LeoMld@users.noreply.github.com>
2020-01-15 19:20:58 +0100

    Update ctrl_valid_teacher.php

--- controller/ctrl_valid_teacher.php
+++ controller/ctrl_valid_teacher.php
@@ -3,7 +3,7 @@
 	require_once('../model/db.php');
 	require_once('../model/security.php');
 	require_once('../model/getTeacher.php');
-
+	//Si on a clique sur valider un prof on obtient donc un id du professeur et on peu modifier en bdd pour indiquer que ce professeur est maintenant vallide
 	$name = getTeacher();
 	if (isset($_POST['id'])){
 		$id = Security::bdd($_POST['id']);
